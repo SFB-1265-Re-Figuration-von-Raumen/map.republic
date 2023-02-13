a5e67be312508bc11eb9c6c3e384bada0ce32756
zloc-alz <alza@zloc.ws>
2023-02-13 16:40:35 +0100

    clg rm

--- pages/icons.js
+++ pages/icons.js
@@ -1,32 +1,27 @@
 import { fetcher } from '@/lib/api'
 import Layout from '../components/Layout'
 
-const Icons = ({ iconlist }) => {
-    console.log(iconlist)
+
+const Icons = ({ icons }) => {
 
     // find all unique categories in the iconlist
-    const categories = iconlist.data.map(icon => icon.attributes.Category)
+    const categories = icons.data.map(icon => icon.attributes.Category)
     const uniqueCategories = [...new Set(categories)]
-    console.log(uniqueCategories)
+    // console.log(uniqueCategories)
 
     // for each unique category, filter the iconlist and create a new array
     const categorizedItems = []
     for (let i = 0; i < uniqueCategories.length; i++) {
-        const filteredIcons = iconlist.data.filter(icon => icon.attributes.Category === uniqueCategories[i])
+        const filteredIcons = icons.data.filter(icon => icon.attributes.Category === uniqueCategories[i])
         categorizedItems.push(filteredIcons)
-        console.log(`logging filtered icons for ${uniqueCategories[i]}`)
-        console.log(filteredIcons)
+        // console.log(`logging filtered icons for ${uniqueCategories[i]}`)
+        // console.log(filteredIcons)
     }
 
     console.log(categorizedItems)
 
-    // 
-
     return (
         <Layout>
-
-            {/* map categorized Items into divs representing each category and containing every item from the current array pos */}
-
             {categorizedItems.map((category, key) => {
                 return (
                     <div key={key}>
@@ -42,21 +37,11 @@ const Icons = ({ iconlist }) => {
                     </div>
                 )
             })}
-
-            {/* {iconlist && iconlist.data?.map((icon, key) => {
-                return (
-                    <>
-                        <img src={icon.attributes.Icon.data.attributes.url} key={key} />
-                        <p key={`p-${key}`}>{icon.attributes.Title}</p>
-                    </>
-                )
-            })} */}
         </Layout>
     )
 }
 
 export default Icons
-
 
 export async function getStaticProps() {
     const iconsResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/icons/?populate=*`,
@@ -64,7 +49,7 @@ export async function getStaticProps() {
     // console.log(iconsResponse)
     return {
         props: {
-            iconlist: iconsResponse
+            icons: iconsResponse
         }
     }
 }
