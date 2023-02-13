8f7396ffc3d4e1f7d569754adfcb756122d54ff6
zloc-alz <alza@zloc.ws>
2023-02-13 15:04:16 +0100

    basic icon fetch

--- pages/icons.js
+++ pages/icons.js
@@ -1,14 +1,18 @@
 import { fetcher } from '@/lib/api'
 import Layout from '../components/Layout'
-// import useSWR from 'swr'
 
 const Icons = ({ iconlist }) => {
-    // const useSWR()
+    console.log(iconlist)
 
     return (
         <Layout>
             {iconlist && iconlist.data?.map((icon, key) => {
-                return <img src={icon.attributes.Icon.data.attributes.url} key={key} />;
+                return (
+                    <>
+                        <img src={icon.attributes.Icon.data.attributes.url} key={key} />
+                        <p key={`p-${key}`}>{icon.attributes.Title}</p>
+                    </>
+                )
             })}
         </Layout>
     )
@@ -20,7 +24,7 @@ export default Icons
 export async function getStaticProps() {
     const iconsResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/icons/?populate=*`,
         { headers: { Authorization: `bearer ${process.env.API_TOKEN}` } })
-    console.log(iconsResponse)
+    // console.log(iconsResponse)
     return {
         props: {
             iconlist: iconsResponse
