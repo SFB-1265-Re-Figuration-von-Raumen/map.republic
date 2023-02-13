37ace10de35636b7affebf91c0c6a2b5f4b83116
zloc-alz <alza@zloc.ws>
2023-02-13 14:52:15 +0100

    add strapi api

--- pages/icons.js
+++ pages/icons.js
@@ -1,11 +1,15 @@
 import { fetcher } from '@/lib/api'
 import Layout from '../components/Layout'
+// import useSWR from 'swr'
 
 const Icons = ({ iconlist }) => {
+    // const useSWR()
 
     return (
         <Layout>
-            <div className="container">hellooo</div>
+            {iconlist && iconlist.data?.map((icon, key) => {
+                return <img src={icon.attributes.Icon.data.attributes.url} key={key} />;
+            })}
         </Layout>
     )
 }
@@ -14,7 +18,7 @@ export default Icons
 
 
 export async function getStaticProps() {
-    const iconsResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/icons/1`,
+    const iconsResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/icons/?populate=*`,
         { headers: { Authorization: `bearer ${process.env.API_TOKEN}` } })
     console.log(iconsResponse)
     return {
