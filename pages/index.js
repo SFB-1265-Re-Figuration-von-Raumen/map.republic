210eaf111e01d3032d2e516710ba0bc61a8b8f21
zloc-alz <alza@zloc.ws>
2023-02-15 15:37:40 +0100

    started with auth, work in progress

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,49 @@
 import { fetcher } from "./api/fetcher";
+import { signOut, useSession } from 'next-auth/react';
 import Image from "next/image";
 import { IconGallery } from "@/components/IconGallery/IconGallery";
+import { useEffect } from "react";
+import Link from "next/link";
 
 export default function Home({ icons }) {
-  return <IconGallery icons={icons} />;
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session == null) return;
+    console.log('session.jwt', session.jwt);
+  }, [session]);
+
+  return (
+    <>
+      <div>
+        <h1>{session ? 'Authenticated' : 'Not Authenticated'}</h1>
+        {session && (
+          <div style={{ marginBottom: 10 }}>
+            <h3>Session Data</h3>
+            <div>Email: {session.user.email}</div>
+            <div>JWT from Strapi: Check console</div>
+          </div>
+        )}
+        {session ? (
+          <button onClick={signOut}>Sign out</button>
+        ) : (
+          <Link href="/auth/sign-in">
+            <button>Sign In</button>
+          </Link>
+        )}
+        <Link href="/protected">
+          <button
+            style={{
+              marginTop: 10,
+            }}
+          >
+            Protected Page
+          </button>
+        </Link>
+      </div>
+      <IconGallery icons={icons} />;
+    </>
+  )
 }
 
 export const getStaticProps = async () => {
